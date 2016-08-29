5b5244a8abe1906c53fb780db052b21f52cf296d
Brandon Tiqui <btiqui@gmail.com>
2016-08-29 15:08:38 -0700

    Add routes to get child and children

--- server.js
+++ server.js
@@ -15,8 +15,10 @@ var childSchema = new Schema({
 
 var Child = mongoose.model('Child', childSchema);
 
+// initially store one child with reward info
+// later refactor to separate table
 var child = new Child({ 
-	name: 'Chloe',
+	name: 'bo',
   goalReason: 'Get out of being grounded.',
   reward: 'Can play outside.',
   starsCurrent: 8,
@@ -26,10 +28,32 @@ var child = new Child({
 /**
 define routes
 **/
-
 app.get('/', function(req, res) {
   res.send('hello world');
 });
+
+// get all children api endpoint
+app.get('/api/children', function(req, res) {
+	Child.find(function (err, children) {
+	  if (err) console.log(err);
+	  res.send(children);
+	});
+});
+
+// get one child api endpoint
+// need to get end of url
+app.get('/api/children/:name', function(req, res) {
+	console.log('get request child: ', req.params.name);
+	Child.findOne({ name: req.params.name}, function (err, child) {
+	  if (err) console.log(err);
+	  console.log('get request child, child found: ', child);
+	  res.send(child);
+	});
+});
+
+// route to increase/decrease stars
+
+// todo: routes to delete child
 
 var port = 3000;
 app.listen(port);
