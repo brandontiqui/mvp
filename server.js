7024b399086969fa6ff5741caf1aeb4c9346e30f
Brandon Tiqui <btiqui@gmail.com>
2016-08-29 12:10:43 -0700

    Add child schema and model

--- server.js
+++ server.js
@@ -1,23 +1,34 @@
 var express = require('express');
 var app = express();
-
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/', function(req, res) {
-  res.send('hello world');
-});
-
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/mvp');
 
-var Cat = mongoose.model('Cat', { name: String });
+var Schema = mongoose.Schema;
 
-var kitty = new Cat({ name: 'Zildjian' });
-kitty.save(function (err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('meow');
-  }
+var childSchema = new Schema({
+  name: String,
+  goalReason: String,
+  reward: String,
+  starsCurrent: Number,
+  starsGoal: Number
+});
+
+var Child = mongoose.model('Child', childSchema);
+
+var child = new Child({ 
+	name: 'Chloe',
+  goalReason: 'Get out of being grounded.',
+  reward: 'Can play outside.',
+  starsCurrent: 8,
+  starsGoal: 10
+}).save();
+
+/**
+define routes
+**/
+
+app.get('/', function(req, res) {
+  res.send('hello world');
 });
 
 var port = 3000;
