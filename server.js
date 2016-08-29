1d2e63f2022e6b8597afccaac62a3888387f8b28
Brandon Tiqui <btiqui@gmail.com>
2016-08-29 11:48:53 -0700

    Verify database is connected

--- server.js
+++ server.js
@@ -6,6 +6,20 @@ app.get('/', function(req, res) {
   res.send('hello world');
 });
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/mvp');
+
+var Cat = mongoose.model('Cat', { name: String });
+
+var kitty = new Cat({ name: 'Zildjian' });
+kitty.save(function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('meow');
+  }
+});
+
 var port = 3000;
 app.listen(port);
 console.log('Server started on port:', port);
