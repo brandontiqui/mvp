23315c1ccf1a42c10ed68a7348eab56dc828a406
Brandon Tiqui <btiqui@gmail.com>
2016-08-29 16:58:50 -0700

    Connect angular view with sample data

--- public/core.js
+++ public/core.js
@@ -1,1 +1,20 @@
-alert('core');
+var myApp = angular.module('myApp',[]);
+
+myApp.controller('AppController', ['$scope', '$http', function($scope, $http) {
+	$scope.name = '';
+	$scope.children = [];
+
+  $http({
+    method: 'GET',
+    url: '/api/children'
+  }).then(function successCallback(response) {
+      // this callback will be called asynchronously
+      // when the response is available
+      console.log(response.data);
+      $scope.children = response.data;
+    }, function errorCallback(response) {
+      // called asynchronously if an error occurs
+      // or server returns response with an error status.
+      console.log('Error getting children: ', response);
+    });
+}]);
