761ba7bc0d71ec1d5114df13ecffa0306edd03e5
Brandon Tiqui <btiqui@gmail.com>
2016-08-29 20:52:35 -0700

    Implement angular count methods

--- public/core.js
+++ public/core.js
@@ -4,32 +4,81 @@ shows all children
 
 var myApp = angular.module('myApp',[]);
 
-myApp.controller('AppController', ['$scope', '$http', function($scope, $http) {
+myApp.factory('myFactory', ['$http', function($http) {
+	var getChild = function(name) { // this may not be name later<-----------
+	  return $http({
+	    method: 'GET',
+	    url: '/api/children/' + name 
+	  });
+	};
+
+	return {
+		getChild: getChild
+	}
+}]);
+
+myApp.controller('AppController', ['$scope', '$http', 'myFactory', function($scope, $http, myFactory) {
 	$scope.name = '';
 	$scope.children = [];
 
-  $http({
-    method: 'GET',
-    url: '/api/children/cho' // hard coded <-----------
-  }).then(function successCallback(response) {
-      // this callback will be called asynchronously
-      // when the response is available
-			$scope.children = []; // clear array
-      console.log('angular get:', response.data);
-      $scope.children.push(response.data);
-    }, function errorCallback(response) {
-      // called asynchronously if an error occurs
-      // or server returns response with an error status.
-      console.log('Error getting children: ', response);
-    });
+	$scope.getChild = function(name) { 
+		myFactory.getChild(name)
+			.then(function success(response) {
+				$scope.children = []; // clear
+				$scope.children.push(response.data);
+			});
+	};
+
+	$scope.getChild('cho'); // <---------hard coded name
 
   // method to increment current count
-  $scope.increment = function(id) {
-  	// send post request to server with _id
-  	// then server will increment the count
+  $scope.raiseCurrent = function(id) {
+	  $http({
+	    method: 'POST',
+	    url: '/cur/inc/' + id 
+	  }).then(function successCallback(response) {
+	  		// get request for child
+	  		$scope.getChild('cho'); // <---------hard coded name
+	    }, function errorCallback(response) {
+	      console.log('Error getting children: ', response);
+	    });
   };
 
+  $scope.lowerCurrent = function(id) {
+  	$http({
+  	  method: 'POST',
+  	  url: '/cur/dec/' + id 
+  	}).then(function successCallback(response) {
+  			// get request for child
+  			$scope.getChild('cho'); // <---------hard coded name
+  	  }, function errorCallback(response) {
+  	    console.log('Error getting children: ', response);
+  	  });
+  };
 
+  $scope.raiseGoal = function(id) {
+  	$http({
+  	  method: 'POST',
+  	  url: '/goal/inc/' + id 
+  	}).then(function successCallback(response) {
+  			// get request for child
+  			$scope.getChild('cho'); // <---------hard coded name
+  	  }, function errorCallback(response) {
+  	    console.log('Error getting children: ', response);
+  	  });
+  };
+
+  $scope.lowerGoal = function(id) {
+  	$http({
+  	  method: 'POST',
+  	  url: '/goal/dec/' + id 
+  	}).then(function successCallback(response) {
+  			// get request for child
+  			$scope.getChild('cho'); // <---------hard coded name
+  	  }, function errorCallback(response) {
+  	    console.log('Error getting children: ', response);
+  	  });
+  };
   // method to decrement count
 }]);
 
--- server.js
+++ server.js
@@ -16,13 +16,13 @@ var Child = mongoose.model('Child', childSchema);
 
 // initially store one child with reward info
 // later refactor to separate table
-var child = new Child({ 
-	name: 'cho',
-  goalReason: 'Get out of being grounded.',
-  reward: 'Can play outside.',
-  starsCurrent: 8,
-  starsGoal: 10
-}).save();
+// var child = new Child({ 
+// 	name: 'cho',
+//   goalReason: 'Get out of being grounded.',
+//   reward: 'Can play outside.',
+//   starsCurrent: 8,
+//   starsGoal: 10
+// }).save();
 
 
 var app = express();
